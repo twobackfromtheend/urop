ba78f17cf4ff2ed9959a96cdd45582640c09afe6
Harry Xie <harry1996@gmail.com>
2019-07-26 21:02:02 +0100

    added ifttt webhook

---
+++ ifttt_webhook.py
@@ -0,0 +1,11 @@
+import os
+import requests
+
+IFTTT_KEY = os.getenv("IFTTT_KEY")
+
+
+def trigger_event(event: str, value1=None, value2=None, value3=None):
+    url = f"https://maker.ifttt.com/trigger/{event}/with/key/{IFTTT_KEY}"
+
+    r = requests.post(url, data={"value1": value1, "value2": value2, "value3": value3})
+    print(r.content)
