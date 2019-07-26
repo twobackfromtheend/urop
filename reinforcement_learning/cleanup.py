71a437d75715fe6e2bb002519b7e6d8ba24db358
Harry Xie <harry1996@gmail.com>
2019-07-26 10:21:23 +0100

    added cleanup to reinforcement learning stuff, updated demonstration with new results.

---
+++ reinforcement_learning/cleanup.py
@@ -0,0 +1,78 @@
+import pickle
+import re
+from collections import deque
+from datetime import datetime
+from operator import attrgetter
+from pathlib import Path
+from typing import NamedTuple
+
+import numpy as np
+
+from reinforcement_learning.Environments.evolving_qubit_env import EvolvingQubitEnv
+
+LOG_FILE = Path(__file__).parent / "logs" / "s_baselines.log"
+RESULTS_FOLDER = Path(__file__).parent / "results"
+
+
+class Protocol(NamedTuple):
+    fidelity: float
+    Omega: np.ndarray
+    Delta: np.ndarray
+
+
+def process_log_file(env: EvolvingQubitEnv):
+    protocols = []
+    with LOG_FILE.open('r') as f:
+        previous_lines = deque(maxlen=10)
+
+        for line in f:
+            previous_lines.append(line)
+
+            if line.startswith("action"):
+                fidelity_achieved_line = previous_lines[-4]
+                fidelity_achieved = float(re.search(r"fidelity_achieved: ([\d.]*),", fidelity_achieved_line).group(1))
+
+                _search = re.search(r"'Omega': ([-\d\[\]\., e]*), 'Delta': ([-\d\[\]\., e]*)", line)
+                Omega = eval(_search.group(1))
+                Delta = eval(_search.group(2))
+
+                Omega += [0]
+                Omega = np.array(Omega)
+                Delta += [Delta[-1]]
+                Delta = np.array(Delta)
+
+                protocols.append(Protocol(fidelity_achieved, Omega, Delta))
+
+    # print(protocols)
+
+    best_protocol = max(protocols, key=attrgetter('fidelity'))
+
+    print(f"Best protocol has fidelity: {best_protocol.fidelity}")
+
+    for _protocol in protocols:
+        if _protocol.fidelity == best_protocol and _protocol is not best_protocol:
+            print(f"Found another protocol with equal fidelity: {_protocol}")
+
+    data = {
+        'evolving_qubit_system_kwargs': env.evolving_qubit_system_kwargs,
+        'protocol': best_protocol,
+    }
+
+    current_datetime = datetime.now().strftime('%Y%m%d_%H%M%S')
+    data_filepath = RESULTS_FOLDER / (current_datetime + ".pkl")
+
+    with data_filepath.open("wb") as f:
+        pickle.dump(data, f, protocol=pickle.HIGHEST_PROTOCOL)
+        print(f"Saved best protocol and data to {data_filepath.name}")
+
+
+if __name__ == '__main__':
+    from qubit_system.geometry.regular_lattice_1d import RegularLattice1D
+    from qubit_system.utils.states import get_ghz_state
+
+    N = 4
+    t = 5
+    env = EvolvingQubitEnv(N=N, V=1, geometry=RegularLattice1D(), t_list=np.linspace(0, t, 20),
+                           ghz_state=get_ghz_state(N))
+
+    process_log_file(env)
--- reinforcement_learning/s_baselines.py
+++ reinforcement_learning/s_baselines.py
@@ -3,11 +3,14 @@ import numpy as np
 
 from stable_baselines.common.policies import MlpPolicy, MlpLstmPolicy
 from stable_baselines.common.vec_env import DummyVecEnv, SubprocVecEnv
-from stable_baselines import PPO2, TRPO, ACKTR
+from stable_baselines import PPO2, TRPO, ACKTR, DDPG
 
 from qubit_system.geometry.regular_lattice_1d import RegularLattice1D
 from qubit_system.utils.states import get_ghz_state
 from reinforcement_learning.Environments.evolving_qubit_env import EvolvingQubitEnv
+from reinforcement_learning.cleanup import process_log_file
+
+gym.logger.setLevel(gym.logger.INFO)
 
 if __name__ == '__main__':
 
@@ -18,12 +21,15 @@ if __name__ == '__main__':
         :param episodes: (int) number of episodes to evaluate
         :return: (float) Mean reward for the last 100 episodes
         """
-        episode_rewards = [0.0]
         obs = env.reset()
+
+        episode_rewards = [0.0]
         total_steps = 0
+        state = None
+        done = [False for _ in range(env.num_envs)]
         while True:
             # _states are only useful when using LSTM policies
-            action, _states = model.predict(obs)
+            action, state = model.predict(obs, state=state, mask=done)
             # here, action, rewards and dones are arrays
             # because we are using vectorized env
             obs, rewards, dones, info = env.step(action)
@@ -47,16 +53,10 @@ if __name__ == '__main__':
         return mean_reward
 
 
-    N = 2
-    t = 1
-    env = EvolvingQubitEnv(N=N, V=1, geometry=RegularLattice1D(), t_list=np.linspace(0, t, 300),
-                           ghz_state=get_ghz_state(N))
-
-
     def make_gym_env():
-        N = 2
-        t = 1
-        env = EvolvingQubitEnv(N=N, V=1, geometry=RegularLattice1D(), t_list=np.linspace(0, t, 300),
+        N = 4
+        t = 10
+        env = EvolvingQubitEnv(N=N, V=1, geometry=RegularLattice1D(), t_list=np.linspace(0, t, 20),
                                ghz_state=get_ghz_state(N))
 
         # env = gym.make('CartPole-v0')
@@ -70,14 +70,19 @@ if __name__ == '__main__':
 
     # model = ACKTR(MlpPolicy, env, verbose=1)
     # model = PPO2(MlpPolicy, env, verbose=1)
-    model = PPO2(MlpLstmPolicy, env,
-                 learning_rate=3e-3,
-                 verbose=1,
-                 nminibatches=1,
-                 policy_kwargs={'n_lstm': 128})
+    model = PPO2(
+        MlpLstmPolicy, env,
+        learning_rate=3e-3,
+        verbose=1,
+        nminibatches=1,
+        # policy_kwargs={'n_lstm': 128},
+        tensorboard_log='./tensorboard_logs'
+    )
 
-    mean_reward = evaluate(model, episodes=10)
+    evaluate(model, episodes=10)
 
-    model.learn(total_timesteps=100000, log_interval=3)
+    model.learn(total_timesteps=600000, log_interval=3)
 
-    mean_reward = evaluate(model, episodes=10)
+    evaluate(model, episodes=10)
+
+    process_log_file(make_gym_env())
