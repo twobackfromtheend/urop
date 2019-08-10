b759bb426efd85b889925543fc6b6b96a953f6aa
Harry Xie <harry1996@gmail.com>
2019-08-10 10:43:36 +0100

    added generic interpolation methods, created demonstration_optimal_control notebook

--- qubit_system/utils/interpolation.py
+++ qubit_system/utils/interpolation.py
@@ -17,4 +17,22 @@ def get_hamiltonian_coeff_linear_interpolation(x: np.array, y: np.array) -> Call
         if t < min(x) or t > max(x):
             return 0
         return f(t)
+
+    return coeff_fn
+
+
+def get_hamiltonian_coeff_interpolation(x: np.array, y: np.array, kind: str) -> Callable[[float], float]:
+    """
+    Clips to 0 beyond x range.
+    :param x:
+    :param y:
+    :return: Function to use as Hamiltonian coefficient
+    """
+    f = interpolate.interp1d(x, y, kind=kind)
+
+    def coeff_fn(t: float, args: dict = None):
+        if t < min(x) or t > max(x):
+            return 0
+        return f(t)
+
     return coeff_fn
