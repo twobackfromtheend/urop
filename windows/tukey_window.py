ba8e2b6d7a6ae8222dfa1cdc58c741d60f5dd510
Harry Xie <harry1996@gmail.com>
2019-11-23 13:59:52 +0000

    added windows

---
+++ windows/tukey_window.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+
+def tukey(timesteps: int, alpha: float):
+    if alpha <= 0 or alpha >= 1:
+        raise ValueError("alpha has to be between 0 and 1 (noninclusive)")
+
+    L = timesteps - 1
+
+    def window_fn(x: float):
+        if x <= 0 or x >= L:
+            return 0
+        if x < alpha * L / 2:
+            return 0.5 * (1 - np.cos(2 * np.pi * x / alpha / L))
+        elif x > timesteps - alpha * L / 2 - 1:
+            return 0.5 * (1 - np.cos(np.pi * (-2 / alpha + 2 * x / alpha / L)))
+        else:
+            return 1
+
+    return window_fn
+
+
+if __name__ == '__main__':
+    from scipy.signal.windows import tukey as scipytukey
+    import matplotlib.pyplot as plt
+
+    timesteps = 500
+    alpha = 0.2
+    scipy_window = scipytukey(timesteps, alpha=alpha)
+
+    x = np.arange(timesteps)
+    plt.plot(x, scipy_window, alpha=0.5)
+
+    window_fn = tukey(timesteps, alpha=alpha)
+    window_fn_array = [window_fn(_x) for _x in x]
+    plt.plot(x, window_fn_array, '--', alpha=0.5)
+
+    print(np.isclose(window_fn_array, scipy_window).all())
+    plt.show()
