b3edfd35b17f3c8f72275dccec3a782541b91e78
Harry Xie <harry1996@gmail.com>
2019-11-24 16:49:34 +0000

    added solver options

--- qutip_job_handlers/solver.py
+++ qutip_job_handlers/solver.py
@@ -1,8 +1,23 @@
+import os
 import numpy as np
-from qutip import *
+from qutip import Options, Qobj, sesolve
+
+options_kwargs = {
+    "num_cpus": os.getenv("NCPUS", 0),
+    "store_final_state": True,
+    "nsteps": 5000,
+    "use_openmp": True
+}
+
+print(f"options kwargs: {options_kwargs}")
+
+qutip_options = Options(**options_kwargs)
+
+print(f"options: {str(qutip_options)}")
 
 
 def solve(hamiltonian: list, psi_0: Qobj, t_list: np.ndarray):
+    # noinspection PyTypeChecker
     return sesolve(
         hamiltonian,
         psi_0,
